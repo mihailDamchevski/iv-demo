3e4defcd337c25d5c03031b59903a9fbfbcdc233
mihailDamchevski <damchmih8@gmail.com>
2026-01-30 13:56:23 +0100

    [test] added test yml and updated playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 2 : undefined,
-  reporter: "allure-playwright",
+  reporter: [["list"], ["allure-playwright"]],
   use: {
     baseURL: "http://localhost:8080/todo",
     headless: process.env.CI ? true : false,
